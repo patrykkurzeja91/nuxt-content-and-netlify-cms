51468ac5907652e8fcda3e1657f5e4064811b3f8
Patryk Kurzeja <patryk.kurzeja@monterail.com>
2021-03-17 12:41:34 +0100

    Initial commit and netlifyCMS setup

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ article.title }}</h1>
+    <p>{{ article.description }}</p>
+    <nuxt-content :document="article" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params, error }) {
+    const article = await $content('articles', params.slug)
+      .fetch()
+
+    return {
+      article
+    }
+  }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    'components/**/*.vue',
+    'layouts/**/*.vue',
+    'pages/**/*.vue',
+    'plugins/**/*.js',
+    'nuxt.config.js'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
